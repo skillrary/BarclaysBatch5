8a9c59ecffe646a24692535e87a9e08697658eb2
SkillRary <46976867+skillrary@users.noreply.github.com>
2020-08-14 16:25:31 +0530

    Create DemoTest.java

---
+++ DemoTest.java
@@ -0,0 +1,21 @@
+package com.barclays;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+
+public class DemoTest {
+
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
